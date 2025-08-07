bba87aafcea0d1488b7e20fa64e0a4829c656edd
Gabriel Oliveira <114035844+BielzinDaAgua@users.noreply.github.com>
2025-08-07 14:09:21 -0300

    .

---
+++ order/cmd/main.go
@@ -0,0 +1,21 @@
+
+
+import (
+	"log"
+
+	"github.com/bielzindaagua/microservices/order/config "
+	"github.com/bielzindaagua/microservices/order/internal/adapters/db"
+
+	"github.com/bielzindaagua/microservices/order/internal/adapters/grpc"
+	"github.com/bielzindaagua/microservices/order/internal/application/core/api"
+)
+
+func main() {
+	dbAdapter, err := db.NewAdapter(config.GetDataSourceURL())
+	if err != nil {
+		log.Fatalf(" Failed to connect to database . Error : %v", err)
+	}
+	application := api.NewApplication(dbAdapter)
+	grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
+	grpcAdapter.Run()
+}
